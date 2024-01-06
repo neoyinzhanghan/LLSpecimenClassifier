cef47313c9e58d96b86efae89a45012f4fe14ba7
Neo Yin <yinzhang@berkeley.edu>
2024-01-05 20:28:01 -0800

    topviews creation script

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pyvips
 
+# pth = "/pesgisipth/NDPI/H23-9432;S14;MSK1 - 2023-12-12 04.55.10.ndpi"
+
+pth = "/media/hdd3/neo/PB_slides/H23-852;S12;MSKW - 2023-06-15 16.42.50.ndpi"
+
 # Load the image
 image = pyvips.Image.new_from_file(
-    "/pesgisipth/NDPI/H23-9432;S14;MSK1 - 2023-12-12 04.55.10.ndpi", level=7
+    pth, level=7
 )
 
 topview = pyvips.Image.new_from_file(
-    "/pesgisipth/NDPI/H23-9432;S14;MSK1 - 2023-12-12 04.55.10.ndpi", level=7
+    pth, level=7
 )
 
 print(image.width, image.height)
