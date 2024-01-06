007f6ab81f7999df9dbcf1b7795054d9ebb11ec9
Neo Yin <yinzhang@berkeley.edu>
2024-01-05 20:22:21 -0800

    topviews creation script

--- test.py
+++ test.py
@@ -5,10 +5,14 @@ image = pyvips.Image.new_from_file(
     "/pesgisipth/NDPI/H23-9432;S14;MSK1 - 2023-12-12 04.55.10.ndpi", level=0
 )
 
+topview = pyvips.Image.new_from_file(
+    "/pesgisipth/NDPI/H23-9432;S14;MSK1 - 2023-12-12 04.55.10.ndpi", level=7
+)
+
 print(image.width, image.height)
 # Crop the image
 # Parameters: left, top, width, height
-cropped_image = image.crop(0, 0, image.width, image.height)
+cropped_image = image.crop(0, 0, topview.width, topview.height)
 
 # print the dimensions of the cropped image
 print(cropped_image.width, cropped_image.height)
