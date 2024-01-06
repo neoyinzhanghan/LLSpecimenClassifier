0a83d524bd81c92fc7e46e0e581403ccc4e72f3f
Neo Yin <yinzhang@berkeley.edu>
2024-01-05 19:36:46 -0800

    topviews creation script

--- get_topviews.py
+++ get_topviews.py
@@ -1,6 +1,7 @@
 import os
 import pyvips
 from tqdm import tqdm
+from pathlib import Path
 
 wsi_dir = "/pesgisipth/NDPI"
 save_dir = "/media/hdd3/neo/all_topviews"
@@ -13,18 +14,10 @@ os.makedirs(save_dir, exist_ok=True)
 
 for file in tqdm(os.listdir(wsi_dir), desc="Getting topviews"):
     if file.startswith("H") or file.startswith("S") and file.endswith(".ndpi"):
-        # Construct the full path for the WSI file
-        wsi_path = os.path.join(wsi_dir, file)
+        # use pyvips to get the topview of each .ndpi file
+        wsi = pyvips.Image.new_from_file(os.path.join(wsi_dir, file), level=0)
 
-        # Load the WSI file at the lowest magnification level
-        wsi = pyvips.Image.new_from_file(wsi_path, level=0)
+        # save the topview as .jpg file in save_dir
+        jpg_name = Path(file).stem + ".jpg"
 
-        level_count = wsi.get("n-pages")
-        lowest_level = level_count - 1  # The lowest magnification level is the last one
-
-        # Construct the save path, removing the original file extension
-        file_name_without_extension = os.path.splitext(file)[0]
-        save_path = os.path.join(save_dir, file_name_without_extension + ".jpg")
-
-        # Save the resized image
-        wsi.write_to_file(save_path)
+        wsi.write_to_file(os.path.join(save_dir, jpg_name))
